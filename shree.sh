47aa0d558ee02f501aa5d595d9b98417decd0a4e
mukeshsirsi <108171264+mukeshsirsi@users.noreply.github.com>
2023-03-13 11:12:53 +0530

    Update shree.sh

--- shree.sh
+++ shree.sh
@@ -4,11 +4,18 @@ sudo yum install -y httpd.x86_64
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
 
-sudo yum update -y
-sudo yum install -y mariadb-server
-sudo systemctl enable mariadb
-sudo systemctl start mariadb
-sudo mysql_secure_installation
+sudo amazon-linux-extras | grep php
+sudo yum-config-manager --disable 'remi-php*'
+sudo amazon-linux-extras enable php8.0
+sudo yum clean metadata
+sudo yum install php-{pear,cgi,pdo,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip
+
+sudo amazon-linux-extras install epel -y
+sudo yum install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm
+sudo yum install mysql-community-server
+sudo systemctl active mysqld
+sudo systemctl start mysqld
+
 
 
 
